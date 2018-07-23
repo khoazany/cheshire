08a5d359a250768980582dfbaf14606ba3abe30b
thundercore-official <github@thundertoken.com>
2018-07-23 16:49:44 -0400

    add thunder network to truffle.js

--- truffle.js
+++ truffle.js
@@ -1,13 +1,21 @@
 const config = require('./config.json')
+const HDWalletProvider = require('truffle-hdwallet-provider')
+const dotenv = require('dotenv')
+
+dotenv.config()
 
 module.exports = {
-  // See <http://truffleframework.com/docs/advanced/configuration>
-  // to customize your Truffle configuration!
   networks: {
     cheshire: {
       host: 'localhost',
       port: config.portTestnet,
       network_id: 1337,
     },
+    thunder: {
+      provider: () => new HDWalletProvider(process.env.MNEMONIC, process.env.THUNDER_RPC, 0, 10),
+      network_id: 19, // Thunder Testnet Network ID
+      gas: 5000000, 
+      gasPrice: 50000000000 // 50 gigaella
+    }
   },
 }
